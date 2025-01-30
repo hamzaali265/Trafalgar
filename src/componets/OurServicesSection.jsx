824315627998014ad7f40ecf83d87549ea689890
hamzaali265 <hafizhamzaali265@gmail.com>
2025-01-30 18:03:40 +0500

    services cards section added

--- src/componets/OurServicesSection.jsx
+++ src/componets/OurServicesSection.jsx
@@ -1,9 +1,52 @@
 import bgShape from "../assets/bgShape.svg";
+import consultation from "../assets/consultation.svg";
+import DetailInfo from "../assets/detailsinfo.svg";
 import dots from "../assets/dots.svg";
+import EmergencyCare from "../assets/emergencycare.svg";
+import pharmacy from "../assets/pharmacy.svg";
+import searchIcon from "../assets/search.svg";
+import Tracking from "../assets/tracking.svg";
 import ServicesCard from "../componets/ServicesCard";
 import "../styles/main_section.css";
 
 function OurServicesSection() {
+  const services = [
+    {
+      img: searchIcon,
+      title: "Search doctor",
+      description:
+        "Choose your doctor from thousands\nof specialist, general, and trusted\nhospitals",
+    },
+    {
+      img: pharmacy,
+      title: "Online pharmacy",
+      description:
+        "Buy  your medicines with our\nmobile application with a simple\ndelivery system",
+    },
+    {
+      img: consultation,
+      title: "Consultation",
+      description:
+        "Free consultation with our trusted\ndoctors and get the best\nrecomendations",
+    },
+    {
+      img: DetailInfo,
+      title: "Details info",
+      description:
+        "Free consultation with our trusted\ndoctors and get the best\nrecomendations",
+    },
+    {
+      img: EmergencyCare,
+      title: "Emergency care",
+      description:
+        "You can get 24/7 urgent care for\nyourself or your children and your\nlovely family",
+    },
+    {
+      img: Tracking,
+      title: "Tracking",
+      description: "Track and save your medical history\nnd health data ",
+    },
+  ];
   return (
     <div style={{ marginTop: "192px" }}>
       <h1
@@ -52,13 +95,14 @@ function OurServicesSection() {
         >
           <img
             src={bgShape}
-            height={"671px"}
+            height={"671.35px"}
             width={"937px"}
-            style={
-              {
-                // position: "absolute"
-              }
-            }
+            style={{
+              opacity: "60%",
+              left: "245px",
+              top: "0.01px",
+              // position: "absolute"
+            }}
           />
           <img src={dots} />
         </div>
@@ -67,14 +111,32 @@ function OurServicesSection() {
           style={{
             zIndex: "1",
             position: "absolute",
-            backgroundColor: "red",
+            // backgroundColor: "red",
+
             width: "100%",
             transform: "translate(-50%, -50%)",
             top: "50%",
             left: "50%",
           }}
         >
-          <ServicesCard />
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              gap: "20px",
+              marginTop: "200px",
+            }}
+          >
+            {services.map((service, index) => (
+              <ServicesCard
+                key={index}
+                img={service.img}
+                title={service.title}
+                description={service.description}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- src/componets/ServicesCard.jsx
+++ src/componets/ServicesCard.jsx
@@ -1,7 +1,39 @@
 function ServicesCard(params) {
+  const { img, title, description } = params;
   return (
-    <div>
-      <h1>sdfsdf</h1>
+    <div
+      style={{
+        height: "300px",
+        width: "300px",
+        backgroundColor: "#ffffff",
+        borderRadius: "20px",
+        boxShadow: "10px 40px 50px 0px rgba(229, 233, 246, 0.4)",
+        padding: "54px 40px 45px 40px",
+      }}
+    >
+      <img src={img} height={90} width={90} style={{ marginBottom: "22px" }} />
+      <p
+        style={{
+          fontFamily: "Mulish",
+          fontSize: "20px",
+          fontWeight: "700",
+          color: "#000000",
+          marginBottom: "22px",
+        }}
+      >
+        {title}
+      </p>
+      <pre
+        style={{
+          fontFamily: "Mulish",
+          fontSize: "16px",
+          fontWeight: "300",
+          color: "#7D7987",
+          lineHeight: "28px",
+        }}
+      >
+        {description}
+      </pre>
     </div>
   );
 }
